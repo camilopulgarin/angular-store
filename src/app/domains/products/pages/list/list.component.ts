84df10245801b1933a133832e7441344b2fbe844
Andres Pulgarin <andres.pulgarin.nopia@gmail.com>
2025-03-24 09:19:24 -0500

    feat: implementacion de pipes y services

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,17 +1,36 @@
-import { Component } from '@angular/core';
-import { ProductComponent } from './../../components/product/product.component';
-import { from } from 'rxjs';
+import { Component, inject, signal } from '@angular/core';
+import { ProductComponent } from '@product/components/product/product.component';
+import { Product } from '@shared/models/product.model';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '@shared/components/header/header.component';
+import { CartService } from '@shared/services/cart.service';
+import { ProductService } from '@shared/services/product.service';
 
 @Component({
   selector: 'app-list',
-  imports: [ProductComponent],
+  imports: [CommonModule, ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-  fromChild(event: string) {
-    console.log('estamos en el padre');
-    console.log(event);
+  products = signal<Product[]>([]);
+  private cartService = inject(CartService)
+  private productService = inject(ProductService);
+
+  ngOnInit() {
+    this.productService.getProducts()
+    .subscribe({
+      next: (products) => {
+        this.products.set(products);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
+  
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
   }
 
 }
---
+++ src/app/domains/shared/components/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject, Input, signal, SimpleChanges } from '@angular/core';
+import { Product } from '../../models/product.model';
+import { CommonModule } from '@angular/common';
+import { CartService } from '../../services/cart.service';
+
+@Component({
+  selector: 'app-header',
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  hideSideMenu = signal(true);
+  private cartService = inject(CartService)
+  cart = this.cartService.cart;
+  total = this.cartService.total;
+
+  toggleSideMenu() {
+    this.hideSideMenu.update(prevState => !prevState);
+  }
+
+}
